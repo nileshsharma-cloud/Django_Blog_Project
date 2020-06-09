5f22d5296f3920bca31aa761777a51aad2dc39a9
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-06-09 23:26:42 +0530

    update code for blog app

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('category', related_name='posts')
+
+    
