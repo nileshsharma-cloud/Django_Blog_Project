e576e4cacdd3ca488324cd9c4e3826f918f1f114
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-06-16 00:03:12 +0530

    Adding code for blog

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -11,4 +11,8 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('category', related_name='posts')
 
-    
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
